675889dc46275ac196f733df282a303871426a27
Andrew Nesvadba <andrew.nesvadba@neds.com>
2024-12-05 20:43:51 +1000

    refactor: day5 part 1

--- src/day05/part1.ts
+++ src/day05/part1.ts
@@ -1,35 +1,28 @@
 // Advent of Code - Day 5 - Part One
 
 export function part1(input: string): number {
-    const [_rules, _pages] = input
+    const [rawRules, rawPagesList] = input
         .replaceAll('\r', '')
         .split('\n\n')
         .map((lines) => lines.split('\n').filter(Boolean));
-    const rules = _rules.reduce((rs, line) => {
+    const rules = rawRules.reduce((rs, line) => {
         const [x, y] = line.split('|').map(Number);
-        rs.after[x] ??= [];
-        rs.after[x].push(y);
-        rs.before[y] ??= [];
-        rs.before[y].push(x);
+        rs[x] ??= [];
+        rs[x].push(y);
         return rs;
-    }, { before: {}, after: {} } as { before: Record<number, number[]>, after: Record<number, number[]> });
-    const pages = _pages.map((page) => page.split(',').map(Number));
-    const middles = pages.filter((page) => {
-        for (const [idx, pageNum] of page.entries()) {
-            for (const [otherIdx, otherPageNum] of page.entries()) {
-                if (idx === otherIdx) continue;
-                if (otherIdx < idx) {
-                    const before = rules.before[pageNum];
-                    if (!before) return false;
-                    if (!before.includes(otherPageNum)) return false;
-                } else {
-                    const after = rules.after[pageNum];
-                    if (!after) return false;
-                    if (!after.includes(otherPageNum)) return false;
+    }, {} as Record<number, number[]>);
+    const pagesList = rawPagesList.map((pages) => pages.split(',').map(Number));
+    const middles = pagesList.filter((pages) => {
+        for (const [idx, page] of pages.entries()) {
+            const pagesAfter = pages.slice(idx + 1);
+            if (!rules[page]) continue;
+            for (const rule of rules[page]) {
+                if (pages.includes(rule) && !pagesAfter.includes(rule)) {
+                    return false;
                 }
             }
         }
         return true;
-    }).map((page) => page[Math.floor(page.length / 2)]);
+    }).map((pages) => pages[Math.floor(pages.length / 2)]);
     return middles.reduce((a, b) => a + b, 0);
 }
--- src/day05/part2.ts
+++ src/day05/part2.ts
@@ -1,10 +1,16 @@
 // Advent of Code - Day 5 - Part Two
 
 export function part2(input: string): number {
-    const items = input
+    const [rawRules, rawPagesList] = input
         .replaceAll('\r', '')
         .split('\n\n')
         .map((lines) => lines.split('\n').filter(Boolean));
-    console.log(items);
+    const rules = rawRules.reduce((rs, line) => {
+        const [x, y] = line.split('|').map(Number);
+        rs[x] ??= [];
+        rs[x].push(y);
+        return rs;
+    }, {} as Record<number, number[]>);
+    const pagesList = rawPagesList.map((pages) => pages.split(',').map(Number));
     return 0;
 }
